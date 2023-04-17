01b8b5b80cf6f8acfcd7abc1236453a5daaf52bb
daanbi1345 <daanbi1345@gmail.com>
2023-04-17 19:50:37 +0900

    2023.04.17

---
+++ connect/connect.php
@@ -0,0 +1,30 @@
+<?php
+    // $host = "localhost";
+    // $user = "daanbi12";
+    // $pw = "chocek12!";
+    // $db = "daanbi12";
+    // $connect = new mysqli($host, $user, $pw, $db);
+    // $connect -> set_charset("utf-8");
+
+    // if(mysqli_connect_errno()){
+    //     echo "Database Connect false";
+    // } else {
+    //     echo "Database Connect True"
+    // }
+?>
+
+<?php
+    $host = "localhost";
+    $user = "root";
+    $pw = "root";
+    $db = "phpClass";
+    $connect = new mysqli($host, $user, $pw, $db);
+    $connect -> set_charset("utf-8");
+
+    if(mysqli_connect_errno()){
+        echo "Database Connect false";
+    } else {
+        // echo "Database Connect True";
+    }
+    
+?>
---
+++ main/main.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP 블로그 만들기</title>
+
+    <?php include "../include/head.php" ?>
+    <!-- head -->
+</head>
+<body class="gray">
+    <?php include "../include/skip.php" ?>
+    <!-- skip -->
+
+    <?php include "../include/header.php" ?>
+    <!-- header -->
+
+    <main id="main" class="container">
+        <div class="intro__inner bmStyle">
+            <picture class="intro__images">
+                <source srcset="../assets/img/intro01.png, ../assets/img/intro01@2x.png 2x, ../joinassets/img/intro01@3x.png 3x" />
+                <img src="../assets/img/intro01.png" alt="소개이미지">
+            </picture> 
+            <p class="intro__text">
+                상상력은 창조의 시발점이다. 
+                당신은 원하는 것을 상상하고 상상하는 것을 행동에 옮길 것이며, 종국에는 행동에 옮길 것을 창조하게 된다.
+            </p>
+        </div>
+        <!-- intro__inner -->
+
+    </main>
+    <!-- //main -->
+</body>
+</html>
